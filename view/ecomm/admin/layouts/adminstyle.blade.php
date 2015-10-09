9c51935a83c16a85594ebd29bde7aba6d36db7f2
Ye' Wint Htat <burmamoenatmin@gmail.com>
2015-10-09 14:41:40 +0630

    Create adminstyle.blade.php

---
+++ view/ecomm/admin/layouts/adminstyle.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Admin</title>
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
